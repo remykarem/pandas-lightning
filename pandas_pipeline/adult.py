2f61462c2ac2136673322f3539f718ce7f3b3c29
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-22 17:06:56 +0800

    Add adult.py

---
+++ pandas_pipeline/adult.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import pandas_pipeline
+
+WORKING_CLASS = {
+    "No income": ['Never-worked', 'Without-pay', 'Self-emp-not-inc'],
+    "Private": ["Private"],
+    "Govt": ['Local-gov', 'State-gov', 'Federal-gov']
+}
+
+EDUCATION = [
+    'Preschool',
+    '1st-4th',
+    '5th-6th',
+    '7th-8th',
+    '9th',
+    '10th',
+    '11th',
+    '12th',
+    'HS-grad',
+    'Assoc-acdm',
+    'Assoc-voc',
+    'Prof-school',
+    'Some-college'
+    'Bachelors',
+    'Masters',
+    'Doctorate',
+]
+
+df = pd.read_csv("/Users/raimibinkarim/Downloads/adult.csv")
+df.optimize.convert_categories()
+df.rename(columns={col: col.replace(".", "_") for col in df})
+df = df.replace("?", np.nan)
+df = df.pipeline.map_categorical_binning(ordered=True, binnings={
+    "workclass": WORKING_CLASS
+})
+df = df.pipeline.as_type({
+    "education_num": "category"
+})
+df = df.pipeline.sapply({
+    "income": lambda s: s == ">50K",
+    "fnlwght": lambda s: s.scaler.standardize(),
+})
+df = df.drop(columns=["education"])
