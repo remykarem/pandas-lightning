ab319cdd821451180f7d4581210ee58e8fa4bfcf
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-22 17:07:19 +0800

    Change API

--- pandas_pipeline/titanic.py
+++ pandas_pipeline/titanic.py
@@ -6,7 +6,7 @@ import pandas_pipeline
 
 df = pd.read_csv(
     "/Users/raimibinkarim/Library/Mobile Documents/com~apple~CloudDocs/Datasets Tabular/titanic.csv")
-df = df.pipeline.convert_dtypes({
+df = df.pipeline.astype({
     "PassengerId": "index",
     "Pclass": CategoricalDtype([3, 2, 1], ordered=True),
     "Sex": "category",
