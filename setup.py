4719b29986880c4b38198d8754cf5683b1794d31
Remy <raimi.bkarim@gmail.com>
2020-07-24 09:09:15 +0800

    Remove colourful

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['numpy>=1.16.1', 'colorful']
+    install_requires=['numpy>=1.16.1']
 )
