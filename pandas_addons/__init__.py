6294c72185388937579eaa9b00a0800d14b08c33
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-28 10:01:23 +0800

    Add standardize in init

--- pandas_addons/__init__.py
+++ pandas_addons/__init__.py
@@ -1,5 +1,7 @@
 from . import dataframe_lambdas, dataframe_optimize, dataframe_dataset
-from . import series_ascii, series_pctg, series_scaler
+from . import series_ascii, series_pctg, series_scaler, series_tests
 from . import series_map_categorical_binning, series_map_numerical_binning
+from . import dataframe_tests
+from .series_scaler import standardize
 
 __version__ = "0.0.1rc0"
