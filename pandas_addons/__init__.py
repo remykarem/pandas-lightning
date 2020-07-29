49bfe05d48759ff9ce9e90e787fa1f4544bf21a2
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-29 20:51:02 +0800

    Add quickplot

--- pandas_addons/__init__.py
+++ pandas_addons/__init__.py
@@ -1,7 +1,7 @@
 from . import dataframe_lambdas, dataframe_optimize, dataframe_dataset
 from . import series_ascii, series_pctg, series_scaler, series_tests
 from . import series_map_categorical_binning, series_map_numerical_binning
-from . import dataframe_tests
+from . import dataframe_tests, dataframe_quickplot
 from .series_scaler import standardize
 
 __version__ = "0.0.1rc0"
