b62756635405e776aeaf8e1909d63fec29983545
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-30 15:34:16 +0800

    Add cramers v

--- pandas_addons/dataframe_tests.py
+++ pandas_addons/dataframe_tests.py
@@ -39,3 +39,42 @@ class tests:
         df = df.iloc[:top]
 
         return df
+
+    def get_cramersv(self):
+
+        def cramers_corrected_stat(confusion_matrix):
+            """ https://stackoverflow.com/a/39266194
+                calculate Cramers V statistic for categorial-categorial association.
+                uses correction from Bergsma and Wicher,
+                Journal of the Korean Statistical Society 42 (2013): 323-328
+            """
+            chi2 = chi2_contingency(confusion_matrix)[0]
+            n = confusion_matrix.sum()
+            phi2 = chi2/n
+            r, k = confusion_matrix.shape
+            phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))
+            rcorr = r - ((r-1)**2)/(n-1)
+            kcorr = k - ((k-1)**2)/(n-1)
+            return np.sqrt(phi2corr / min((kcorr-1), (rcorr-1)))
+
+        cols = self._obj.select_dtypes(include=["category", "bool"]).columns.tolist()
+        if len(cols) != len(set(cols)):
+            raise ValueError("Col names must be unique!")
+        print(f"Testing {str(cols)}")
+
+        pairs = combinations(cols, 2)
+
+        cramers = []
+        for pair in pairs:
+            col1, col2 = pair
+            confusion_matrix = pd.crosstab(
+                self._obj[col1], self._obj[col2]).values
+            coeff = cramers_corrected_stat(confusion_matrix)
+            cramers.append(tuple([col1, col2, coeff]))
+
+        cramers = pd.DataFrame(
+            cramers, columns=["col1", "col2", "coefficient"])
+        cramers.sort_values(by="coefficient", ascending=False,
+                            ignore_index=True, inplace=True)
+
+        return cramers
