83a7e5b2120076b2b33c12ec83870a804c0eb15b
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-30 20:26:03 +0800

    Remove sphinx temporarily

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=["pandas",
-                      "sphinx",
-                      "sphinx-rtd-theme",
+    install_requires=["pandas>=0.21.0",
+                    #   "sphinx",
+                    #   "sphinx-rtd-theme",
                       "scipy",
                       ]
 )
