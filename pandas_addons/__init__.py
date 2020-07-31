fcb866642ec1a748b34b1c360812944dda83ce3c
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-07-31 18:01:57 +0800

    Update __init__

--- pandas_addons/__init__.py
+++ pandas_addons/__init__.py
@@ -1,5 +1,5 @@
 from . import dataframe_lambdas, dataframe_optimize, dataframe_dataset
-from . import series_ascii, series_pctg, series_scaler, series_tests
+from . import series_asciiplot, series_pctg, series_scaler, series_tests
 from . import series_map_categorical_binning, series_map_numerical_binning
 from . import dataframe_tests, dataframe_quickplot
 from .series_scaler import standardize
