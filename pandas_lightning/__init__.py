4b878d6bce4d829ecff89b2c241fcf4e0402543d
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-08-13 21:08:14 +0800

    Rename to dtpp

--- pandas_lightning/__init__.py
+++ pandas_lightning/__init__.py
@@ -1,7 +1,7 @@
 from . import dataframe_lambdas, dataframe_optimize, dataframe_dataset
 from . import series_asciiplot, series_pctg, series_scaler, series_tests
 from . import series_map_categorical_binning, series_map_numerical_binning
-from . import series_vec
+from . import series_dtpp
 from . import dataframe_tests, dataframe_quickplot
 from .series_scaler import standardize
 from .pipeline import Pipeline
