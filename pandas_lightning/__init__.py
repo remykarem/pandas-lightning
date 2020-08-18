b3a316024c346cff717ee916100203c6d513cfe7
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-08-18 22:17:00 +0800

    Rearrange imports

--- pandas_lightning/__init__.py
+++ pandas_lightning/__init__.py
@@ -1,9 +1,10 @@
-from . import dataframe_lambdas, dataframe_optimize, dataframe_dataset
-from . import series_asciiplot, series_pctg, series_scaler, series_tests
+from . import dataframe_dataset, dataframe_lambdas, dataframe_optimize
+from . import dataframe_quickplot, dataframe_tests
+from . import series_asciiplot, series_datetime
 from . import series_map_categorical_binning, series_map_numerical_binning
-from . import series_dtpp
-from . import dataframe_tests, dataframe_quickplot
+from . import series_pctg, series_scaler, series_tests
 from .series_scaler import standardize
 from .pipeline import Pipeline
+from ._utils import makeDataFrame
 
 __version__ = "0.0.1rc0"
