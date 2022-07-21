6c86d82db0a5c4a48127a41c0cf006f6a8618190
remykarem <raimi.bkarim@gmail.com>
2022-07-21 22:55:32 +0800

    Set SNS theme

--- pandas_lightning/__init__.py
+++ pandas_lightning/__init__.py
@@ -6,5 +6,8 @@ from . import series_map_categorical_binning, series_map_numerical_binning
 from . import series_pctg, series_scaler, series_tests
 from .series_scaler import standardize
 from ._utils import makeDataFrame
+import seaborn as sns
 
 __version__ = "0.0.1"
+
+sns.set_theme()
